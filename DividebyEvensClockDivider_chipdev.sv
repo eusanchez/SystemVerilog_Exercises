e644992bce3cfda9b56eb376878b9c933f330304
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2026-01-21 22:03:36 +0000

    clock divider exercise

--- DividebyEvensClockDivider_chipdev.sv
+++ DividebyEvensClockDivider_chipdev.sv
@@ -6,17 +6,25 @@ module model (
   output logic div6
 );
 
+logic counter2;
+logic [1:0] counter4;
+int counter6;
+
 always_ff @(posedge clk) begin
-  if(resetn) begin
-    {div2,div4, div6} <= '0;
+  if(!resetn) begin
+    counter2 <= '0;
+    counter4 <= '0;
+    counter6 <= '0;
+  end else begin
+    counter2 <= counter2 + 1;
+    counter4 <= counter4 + 1;
+    counter6 <= (counter6 + 1) % 6;
   end
 end
 
-task automatic clock_gen();
-
-endtask
-
-
+assign div2 = (counter2 == 1);
+assign div4 = (counter4 == 1) || (counter4 == 2);
+assign div6 = (counter6 == 1) || (counter6 == 2) || (counter6 == 3);
 
 
 endmodule
