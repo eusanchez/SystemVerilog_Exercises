b44b5f44a2de908e240c383ca30f12a4d129771a
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2026-01-25 20:38:57 +0000

    fifo code

--- FIFO/top.sv
+++ FIFO/top.sv
@@ -3,8 +3,8 @@ module fifo #(parameter DEPTH = 32,
               parameter WIDTH = 16
 ) 
 (
-    input logic clk_i,
-    input logic rst_n_i,
+    input logic clk,
+    input logic rst,
 
     //--------------------------------
     // Write
@@ -31,8 +31,31 @@ logic full, empty;
 // They need to be of size ADDR_WIDTH since pointers are picking a specific address as its victim.
 logic [ADDR_WIDTH-1:0] read_pointer, write_pointer;
 
-logic [WIDTH-1:0] FIFO [DEPTH];
+logic [WIDTH-1:0] fifo [DEPTH];
 
+always_ff @(posedge clk) begin
+    if(!rst) begin
+        write_pointer <= '0;
+    end else begin
+        if (wr_en_i & !full) begin
+            fifo[write_pointer] <= wdata_i;
+            write_pointer <= write_pointer + 1;
+        end
+    end 
+end
 
+always_ff @(posedge clk) begin
+    if(!rst) begin
+        read_pointer <= '0;
+    end else begin
+        if (rd_en_i & !empty) begin
+            rdata_o <= fifo[read_pointer];
+            read_pointer <= read_pointer + 1;
+        end
+    end
+end
+
+assign full = ((write_pointer + 1) == read_pointer); // if 1, it means it has no space to write, it has DEPTH valid entries being occupied.
+assign empty = (write_pointer == read_pointer); // if 1, it means fifo has zero valid entries, meaning that it has nothing to read.
 
 endmodule
