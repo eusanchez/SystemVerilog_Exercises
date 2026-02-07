67080e40992f704cbb45fcec5567ea21c712ad8f
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2026-02-07 23:19:35 +0000

    updated code

--- ProgrammableSequenceDetector_chipdev.sv
+++ ProgrammableSequenceDetector_chipdev.sv
@@ -6,73 +6,17 @@ module model (
   output logic seen
 );
 
-parameter BIT0 = 0,
-          BIT1 = 1,
-          BIT2 = 2,
-          BIT3 = 3,
-          BIT4 = 4,
-          IDLE = 5;
+logic [4:0] correct_pattern; 
+logic [4:0] seq;
 
-logic [2:0] state, next_state;
-
-logic first, second;
-
-always_ff @(posedge clk) begin
+always @(posedge clk) begin
   if(!resetn) begin
-    state <= IDLE;
+    seq <= '0;
+    correct_pattern <= init;
   end else begin
-    state <= next_state;
+    seq <= {seq, din};
   end
 end
 
-always_comb begin
-  case(state) 
-    IDLE: begin
-      if(resetn) begin
-        next_state = BIT0;
-      end else begin
-        next_state = IDLE;
-      end
-    end
-    BIT0: begin
-      if(init[0] == din) begin
-        first = din;
-        next_state = BIT1;
-      end else begin
-        next_state = BIT0;
-      end
-    end
-    BIT1: begin
-      if(init[1] == din) begin
-        second = din;
-        next_state = BIT2;
-      end else begin
-        next_state = BIT1;
-      end
-    end
-    BIT2: begin
-      if(init[2] == din) begin
-        next_state = BIT3;
-      end else if (first == din) begin
-        next_state = BIT1;
-      end else begin
-        next_state = BIT0;
-      end
-    end
-    BIT3: begin
-      if(init[3] == din) begin
-        next_state = BIT4;
-      end else begin
-        next_state = BIT0;
-      end
-    end
-    BIT4:
-      if(init[4] == din) begin
-        next_state = IDLE;
-        seen = '1;
-      end
-    default: next_state = IDLE;
-  endcase
-end
-
+assign seen = (seq == correct_pattern) ? '1 : '0;
 endmodule
